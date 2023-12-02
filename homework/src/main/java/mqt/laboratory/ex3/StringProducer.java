30c567610a766ac37cbd04c52dc41c4da729a4ee
Sebastian Iscruleasa <sebi.iscruleasa@yahoo.ro>
2023-12-02 18:12:07 +0200

    exercise 3

---
+++ homework/src/main/java/mqt/laboratory/ex3/StringProducer.java
@@ -0,0 +1,35 @@
+package mqt.laboratory.ex3;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class StringProducer {
+    private static final String OUR_BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String OUR_CLIENT_ID = "stringProducer";
+
+    private static Producer<String, String> producer;
+
+    public static void main(String[] args){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, OUR_BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, OUR_CLIENT_ID);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        producer = new KafkaProducer<>(props);
+
+        ProducerRecord<String, String> data1 = new ProducerRecord<>("topic1", "Testing 1");
+
+        ProducerRecord<String, String> data2 = new ProducerRecord<>("topic1",  "Testing 2");
+        try {
+            producer.send(data1).get();
+            producer.send(data2).get();
+        } catch (InterruptedException | ExecutionException e){
+            producer.flush();
+        }
+        producer.close();
+    }
+}
