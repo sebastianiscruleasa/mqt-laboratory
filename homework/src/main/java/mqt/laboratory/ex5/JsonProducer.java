c96489cb5bce140b0825f2370c417ac414d4431d
Sebastian Iscruleasa <sebi.iscruleasa@yahoo.ro>
2023-12-02 20:13:42 +0200

    exercise 5

---
+++ homework/src/main/java/mqt/laboratory/ex5/JsonProducer.java
@@ -0,0 +1,39 @@
+package mqt.laboratory.ex5;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class JsonProducer {
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String CLIENT_ID = "stringProducer";
+
+    private static Producer<String, String> producer;
+
+    public static void main(String[] args){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        producer = new KafkaProducer<>(props);
+
+        String json="{\n" +
+                "    \"nume\":\"Iscruleasa\",\n" +
+                "    \"prenume\":\"Sebastian\",\n" +
+                "    \"varsta\":\"22 ani\",\n" +
+                "    \"hobby\":\"fotbal\"\n" +
+                "}";
+
+        ProducerRecord<String, String> data1 = new ProducerRecord<>("topic1", json);
+        try {
+            producer.send(data1).get();
+        } catch (InterruptedException | ExecutionException e){
+            producer.flush();
+        }
+        producer.close();
+    }
+}
