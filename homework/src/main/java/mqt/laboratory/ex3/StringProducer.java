3bf790a973713eea136347247b57ea416f288e0f
Sebastian Iscruleasa <sebi.iscruleasa@yahoo.ro>
2023-12-02 19:09:15 +0200

    exercise 4

--- homework/src/main/java/mqt/laboratory/ex3/StringProducer.java
+++ homework/src/main/java/mqt/laboratory/ex3/StringProducer.java
@@ -7,15 +7,15 @@ import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class StringProducer {
-    private static final String OUR_BOOTSTRAP_SERVERS = "localhost:9092";
-    private static final String OUR_CLIENT_ID = "stringProducer";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String CLIENT_ID = "stringProducer";
 
     private static Producer<String, String> producer;
 
     public static void main(String[] args){
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, OUR_BOOTSTRAP_SERVERS);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, OUR_CLIENT_ID);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
